2e79b191404419ff2e7ab13981c8659e705f1a30
董文强 <826112946@qq.com>
2019-01-04 16:34:49 +0800

    ttt

---
+++ src/main/java/org/sd/user/RabbitConsumerApp.java
@@ -0,0 +1,21 @@
+package org.sd.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author 董文强
+ * @version 1.0
+ * @date 2018年12月18日
+ */
+@SpringBootApplication
+public class RabbitConsumerApp {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitConsumerApp.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitConsumerApp.class, args);
+    }
+}
